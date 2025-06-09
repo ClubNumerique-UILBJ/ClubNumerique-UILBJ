fafd29268692865a309acffd120e72738200d6a7
ClubNumerique-UILBJ <clubnumeriqueuilbj@gmail.com>
2025-06-09 22:37:23 +0100

    Delete clubnumerique-uilbj directory

--- clubnumerique-uilbj/page-com/minitube/actions/add_comment.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../db.php';
-$tuto_id = $_POST['id'];
-$msg = $conn->real_escape_string($_POST['message']);
-$conn->query("INSERT INTO commentaires (tuto_id, message) VALUES ($tuto_id, '$msg')");
-header("Location: ../index.php");
-?>
--- clubnumerique-uilbj/page-com/minitube/actions/like.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../db.php';
-$id = $_POST['id'];
-$conn->query("UPDATE tutos SET likes = likes + 1 WHERE id = $id");
-header("Location: ../index.php");
-?>
